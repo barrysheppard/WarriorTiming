b35bcebd9c0b9556daa84a5f9e1470bccce219f6
Barry Sheppard <barrysheppard@gmail.com>
2020-10-03 23:46:51 +0100

    Tidied up the functions and added additional cooldowns

--- load.py
+++ load.py
@@ -18,6 +18,25 @@ import math
 ###############################################################################
 
 
+def boss_percentage(time_mark, raid_id, boss_id):
+        start = time_mark
+        end = time_mark + 1000 # one second after
+        events = load_events(str(raid_id), str(start), str(end), str(boss_id))
+        event_lines = events['events']
+        for x in event_lines:
+            if  'hitPoints' in x :
+                return(str(x['hitPoints']))
+                break
+        else:
+            return(100)
+
+
+def check_log(log_id):
+    """The primary piece of code that is called"""
+    combatlog = load_combatlog(log_id)
+    print_fights(combatlog, log_id)
+
+
 def load_combatlog(log_id):
     """Return combat log"""
     api_id = "2ffbd9cca71b2ba1ffa76671d71a4c29"
@@ -32,21 +51,6 @@ def load_combatlog(log_id):
     else:
         data = json.loads(r.content.decode())
     return(data)
-
-
-def print_fights(log_id, id):
-    """Return list of bosses from the log"""
-    fights = log_id['fights']
-    for x in fights:
-        if x['boss'] > 0:
-            time = (x['end_time'] - x['start_time']) / 1000
-            time = math.floor(time)
-            text_one = str(x['id']) + " " + x['name'] + " : " + str(time) + " seconds"
-            start_deathwish = x['end_time']-31000
-            events = load_events(str(id), str(start_deathwish), str(x['end_time']), str(x['id']))
-            deathwish = event_times(events)
-            text_two = " Deathwish on: " + str(deathwish) + "%"
-            print(text_one + text_two)
 
 
 def load_events(id, start_time, end_time, targetID):
@@ -64,12 +68,30 @@ def load_events(id, start_time, end_time, targetID):
             data = json.loads(r.content.decode())
         return(data)
 
-def event_times(events):
-    """Prints event times"""
-    event_lines = events['events']
-    for x in event_lines:
-        if  'hitPoints' in x:
-            return(str(x['hitPoints']))
+
+def print_fights(log, log_id):
+    """Return list of bosses from the log"""
+    fights = log['fights']
+    for x in fights:
+        if x['boss'] > 0 and x['kill'] == True :
+            time = (x['end_time'] - x['start_time']) / 1000
+            time = math.floor(time)
+            text_one = x['name'] + " : " + str(time) + " seconds"
+            # Diamond flask is 60 seconds
+            start_diamondflask = x['end_time']-60000
+            boss_percent = boss_percentage(start_diamondflask, log_id, x['id'])
+            text_two = ", Diamond Flask on: " + str(boss_percent) + "%"
+            # Deathwish is 30 seconds
+            start_deathwish = x['end_time']-30000
+            boss_percent = boss_percentage(start_deathwish, log_id, x['id'])
+            text_three = ", Deathwish on: " + str(boss_percent) + "%"
+            # Mighty Rage Pot / Rek is 20 seconds
+            start_mragepot = x['end_time']-20000
+            boss_percent = boss_percentage(start_mragepot, log_id, x['id'])
+            text_four = ", Mighty Rage Pot / Rek on: " + str(boss_percent) + "%"
+
+            print(text_one + text_two + text_three + text_four)
+
 
 ###############################################################################
 # Code for when file is run from command line                                 #
@@ -77,31 +99,4 @@ def event_times(events):
 
 if __name__ == '__main__':
 
-    combatlog = load_combatlog("6T9jDbqBH8cnzVxY")
-    print_fights(combatlog, "6T9jDbqBH8cnzVxY")
-
-#    end_time = 1655297
-#    deathwish = end_time - 31000
-#    events = load_events("6T9jDbqBH8cnzVxY", deathwish, end_time, 16)
-#    event_times(events)
-
-
-
-#{'events':
-#[{'timestamp': 1590468, 'type': 'damage', 'sourceID': 15, 'sourceIsFriendly': True,
-#  'targetID': 64, 'targetIsFriendly': False, 'ability': {'name': 'Heroic Strike', 'guid': 25286,
-#  'type': 1, 'abilityIcon': 'ability_rogue_ambush.jpg'}, 'buffs': '22817.24425.22888.', 'hitType': 1,
-#  'amount': 282, 'mitigated': 191, 'unmitigatedAmount': 473, 'resourceActor': 2,
-#  'classResources': [{'amount': 0, 'max': 0, 'type': -1}],
-#  'hitPoints': 100, 'maxHitPoints': 100, 'attackPower': 0, 'spellPower': 0, 'armor': 0,
-#  'x': -121200, 'y': -805230, 'facing': -210, 'mapID': 0, 'itemLevel': 63, 'sourceMarker': 6
-# },
-# {'timestamp': 1590487, 'type': 'damage', 'sourceID': 15, 'sourceIsFriendly': True, 'targetID': 64,
-#  'targetIsFriendly': False, 'ability': {'name': 'Bloodthirst', 'guid': 23894, 'type': 1,
-#  'abilityIcon': 'spell_nature_bloodlust.jpg'}, 'buffs': '22817.24425.22888.', 'hitType': 8,
-#  'amount': 0, 'sourceMarker': 6}, {'timestamp': 1590837, 'type': 'damage', 'sourceID': 12,
-#  'sourceIsFriendly': True, 'targetID': 64, 'targetIsFriendly': False,
-#  'ability': {'name': 'Melee', 'guid': 1, 'type': 1, 'abilityIcon': 'inv_axe_02.jpg'},
-#  'buffs': '24425.24932.22888.', 'hitType': 6, 'amount': 192, 'mitigated': 155,
-#  'unmitigatedAmount': 347, 'resourceActor': 2, 'classResources': [{'amount': 0, 'max': 0, 'type': -1}], 'hitPoints': 100, 'maxHitPoints': 100, 'attackPower': 0, 'spellPower': 0, 'armor': 0, 'x': -121200, 'y': -805230, 'facing': -210, 'mapID': 0, 'itemLevel': 63}, {'timestamp': 1591246, 'type': 'damage', 'sourceID': 15, 'sourceIsFriendly': True, 'targetID': 64, 'targetIsFriendly': False, 'ability': {'name': 'Melee', 'guid': 1, 'type': 1, 'abilityIcon': 'inv_axe_02.jpg'}, 'buffs':
-#  '22817.24425.24932.22888.', 'hitType': 2, 'amount': 319, 'unmitigatedAmount': 267, 'resourceActor': 2, 'classResources': [{'amount': 0, 'max': 0, 'type': -1}], 'hitPoints': 100,
+    check_log("8AkzDGqNW4RFLgma")
